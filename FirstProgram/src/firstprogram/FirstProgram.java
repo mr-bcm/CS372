894fd4788a797a7e9e5d27c9dc2c32b3e3894a2b
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-05 14:59:07 -0800

    Spanish Version

--- FirstProgram/src/firstprogram/FirstProgram.java
+++ FirstProgram/src/firstprogram/FirstProgram.java
@@ -15,7 +15,7 @@ public class FirstProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hola Mundo");
     }
     
 }
