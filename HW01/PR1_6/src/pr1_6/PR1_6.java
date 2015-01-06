bf9807d54cc3d484538555dfbba37e870a1438d9
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-06 12:21:37 -0800

    Added PR1_6

---
+++ HW01/PR1_6/src/pr1_6/PR1_6.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr1_6;
+import java.util.Scanner;
+
+/**
+ * HW01<br>
+ * PR1_6: Prime Numbers
+ *
+ * @author Brennan Metzelaar
+ */
+public class PR1_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double num = 0;
+        Scanner input = new Scanner(System.in);
+        num = input.nextDouble();
+        
+        for(int i = 0; i < num; i++){
+            for (int j = 0; j < 100; j++){
+                
+            }
+        }
+    }
+    
+}
