0aa583d19748e564d4954b0ba4c519910d5dec6b
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-07 12:22:55 -0800

    Small Changes
    Setting up PR2_3 based off of the earlier gerbil problems.

---
+++ HW02/PR2_3/src/pr2_3/Gerbil.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr2_3;
+
+/**
+ * A gerbil is assigned a number and has various functions, such as being able to hop.
+ * @author Brennan
+ * @since 1/7/2015
+ */
+public class Gerbil {
+    private int gerbilNumber;
+    
+//    /**
+//     * Default constructor initializes the number of gerbils to 5.
+//     */
+//    public Gerbil(){ gerbilNumber = 5; }
+    
+    /**
+     * Initialize the number that represents this Gerbil.
+     * @param x the number of gerbils
+     */
+    public Gerbil(int x){ gerbilNumber = x; }
+    
+    /**
+     * Outputs that g gerbil is jumping
+     * @param g the number of the gerbil in question
+     */
+    public static void hop(Object g){
+        Gerbil gerbil = (Gerbil)g;
+        System.out.println("Gerbil #" + gerbil.getNum() + " is hopping!");
+    }
+    
+    /**
+     * The number of the current gerbil is returned
+     * @return an integer that represents the gerbil's number
+     */
+    public int getNum(){ return gerbilNumber; }
+}
--- HW02/PR2_3/src/pr2_3/PR2_3.java
+++ HW02/PR2_3/src/pr2_3/PR2_3.java
@@ -6,17 +6,39 @@
  */
 package pr2_3;
 
+import java.util.*;
+
 /**
- *
+ * Main class that executes the Gerbil program.
  * @author Brennan
+ * @since 1/7/2015
  */
 public class PR2_3 {
 
     /**
-     * @param args the command line arguments
+     * A List is populated that consists of Gerbil objects.
+     * <p>
+     * The function takes in the user input of how many gerbils they want.
+     * Then each gerbil is given a number and is then told to hop.
+     * The number of each gerbil is output to the user.
+     *
+     * @param args There is no supported use of additional arguments.
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int size = 0;   // the total number of gerbils to itterate through
+        List manyGerbils = new ArrayList();
+
+        System.out.println("How many Gerbils are there?");
+        Scanner input = new Scanner(System.in);
+        size = input.nextInt();
+        System.out.println("All of the gerbils have started hopping!?");
+
+        for (int i = 0; i < size; i++) {
+            manyGerbils.add(new Gerbil(i));
+        }
+        for (int i = 0; i < manyGerbils.size(); i++) {
+            Gerbil.hop(manyGerbils.get(i));
+        }
     }
-    
+
 }
