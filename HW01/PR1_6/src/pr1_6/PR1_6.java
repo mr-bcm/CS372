13f5a70bbcd5ebf62d201f2b6bf33c2b9c39c553
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-06 18:20:57 -0800

    Finished PR1_6

--- HW01/PR1_6/src/pr1_6/PR1_6.java
+++ HW01/PR1_6/src/pr1_6/PR1_6.java
@@ -5,25 +5,23 @@
  */
 package pr1_6;
 
-import java.util.Scanner;
 
 /**
- * HW01<br>
- * PR1_6: Prime Numbers
+ * Calculates the numbers between 1-100 that are prime.
  *
  * @author Brennan Metzelaar
  */
 public class PR1_6 {
 
     /**
+     * Displays the prime numbers between 1-100 on screen.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         boolean isPrime = true;
-        int num;
-        //Scanner input = new Scanner(System.in);
-        //num = input.nextDouble();
 
+        System.out.println("The following numbers are prime:");
         for (int i = 1; i < 101; i++) {
             isPrime = true;
             for (int j = 2; j < i; j++) {
@@ -36,5 +34,4 @@ public class PR1_6 {
             }
         }
     }
-
 }
