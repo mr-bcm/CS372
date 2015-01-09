55d820e162ea7477afe17e222be2830632b7424a
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-09 14:24:38 -0800

    Created PR2_7
    GUI is almost done, coding next.

---
+++ HW02/PR2_7/src/pr2_7/PR2_7.java
@@ -0,0 +1,23 @@
+/*
+ * HW02-PR2_7: Cookie Sales
+ * By: Brennan Metzelaar
+ * Edited: 1/9/2015
+ * Due: 1/9/2015
+ */
+
+package pr2_7;
+
+/**
+ *
+ * @author bmetzelaar16
+ */
+public class PR2_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
