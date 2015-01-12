b631ca211173e43f5da64262b483e1e43ca3392d
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-12 09:37:58 -0800

    Updated PR3_1
    Was originally using ValueRange from PR2_6 to get min, max, and average.
    The issue has to do with me using integers here rather than doubles. For
    now I have just added a copy of the class called Range with 3 more
    methods that support min, max, and average for integer values. I hope to
    just convert he class to work with generic type T if I can. Looking into
    it.

--- HW03/PR3_1/src/pr3_1/PR3_1.java
+++ HW03/PR3_1/src/pr3_1/PR3_1.java
@@ -5,6 +5,8 @@
  */
 package pr3_1;
 
+import java.util.*; // Scanner, List, ListArray
+
 /**
  *
  * @author Brennan
@@ -15,7 +17,23 @@ public class PR3_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List l = new ArrayList();
+        // int[] arr = new int[10];
+        Range vr = new Range();
+        
+
+        System.out.println("Enter 10 numbers:");
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            l.add(input.nextInt());
+            
+            // arr[i] = input.nextInt();
+        }
+        System.out.println("Here is some data about your numbers:");
+        System.out.printf("The max number is %d\n", (int)vr.getMaxInt(l));
+        System.out.printf("The min number is %d\n", (int)vr.getMinInt(l));
+        System.out.printf("The average number is %d\n", (int)vr.getAverageInt(l));
+
+        
     }
-    
 }
---
+++ HW03/PR3_1/src/pr3_1/Range.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr3_1;
+import java.util.*;
+
+/**
+ * Contains methods for calculating the minimum, maximum, and average values
+ * within a List.
+ * <p> There are two sets of methods, the default is for double values (getMin) 
+ * and the methods with Int in the name are for getting integer values 
+ * (getMinInt).
+ * 
+ * @author Brennan
+ */
+public class Range {
+
+    /**
+     * Returns the smallest double value that was found within a given List.
+     * 
+     * @param l represents a list that contains double variables
+     * @return returns the minimum double value that appeared in the List
+     */
+    public double getMin(List l) {
+        double min = Double.MAX_VALUE;
+
+        for (int i = 0; i < l.size(); i++) {
+            if (min > (double) l.get(i)) {
+                min = (double) l.get(i);
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Returns the largest double value that was found within a given List.
+     * 
+     * @param l represents a list that contains double variables
+     * @return returns the maximum double value that appeared in the List
+     */
+    public double getMax(List l) {
+        double max = Double.MIN_VALUE;
+        
+        for (int i = 0; i < l.size(); i++){
+            if (max < (double) l.get(i)){
+                max = (double) l.get(i);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Returns the average double value that was found within a given List.
+     * 
+     * @param l represents a list that contains double variables
+     * @return returns the average double value that appeared in the List
+     */
+    public double getAverage(List l) {
+        double sum = 0;
+
+        for (int i = 0; i < l.size(); i++) {
+            sum += (double) l.get(i);
+        }
+        return sum / (double) l.size();
+    }
+
+    // BELOW IS FOR INTEGER VALUES
+    
+    /**
+     * Returns the smallest integer value that was found within a given List.
+     * 
+     * @param l represents a list that contains integer variables
+     * @return returns the minimum integer value that appeared in the List
+     */
+    public int getMinInt(List l) {
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < l.size(); i++) {
+            if (min > (int)l.get(i)) {
+                min = (int)l.get(i);
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Returns the largest integer value that was found within a given List.
+     * 
+     * @param l represents a list that contains integer variables
+     * @return returns the maximum integer value that appeared in the List
+     */
+    public int getMaxInt(List l) {
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < l.size(); i++){
+            if (max < (int) l.get(i)){
+                max = (int) l.get(i);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Returns the average integer value that was found within a given List.
+     * 
+     * @param l represents a list that contains integer variables
+     * @return returns the average integer value that appeared in the List
+     */
+    public int getAverageInt(List l) {
+        int sum = 0;
+
+        for (int i = 0; i < l.size(); i++) {
+            sum += (int) l.get(i);
+        }
+        return sum / (int) l.size();
+    }
+}
