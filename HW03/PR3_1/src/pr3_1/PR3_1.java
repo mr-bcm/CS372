2cc95de8cb7b9f0d4dfeb203344069ef16808362
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-12 12:16:50 -0800

    PR3_1 Added Try Catch Statements

--- HW03/PR3_1/src/pr3_1/PR3_1.java
+++ HW03/PR3_1/src/pr3_1/PR3_1.java
@@ -4,43 +4,49 @@
  * Edited: 1/12/2015
  * Due: 1/13/2015
  */
-
 package pr3_1;
 
 import java.util.*; // Scanner, List, ListArray
 
 /**
- * Main class - Get user input of 10 integers and calculate the max, min, and 
+ * Main class - Get user input of 10 integers and calculate the max, min, and
  * average.
- * 
+ *
  * @author Brennan
  */
 public class PR3_1 {
 
     /**
-     * Get user input of 10 integers and output the max, min, and average 
-     * values of the 10 integers.
+     * Get user input of 10 integers and output the max, min, and average values
+     * of the 10 integers.
+     *
      * @param args not supported
      */
     public static void main(String[] args) {
         List l = new ArrayList();
         // int[] arr = new int[10];
         Range vr = new Range();
-        
 
         System.out.println("Enter 10 numbers:");
         Scanner input = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
-            l.add(input.nextInt());
-            
-            // arr[i] = input.nextInt();
-        }
-        System.out.println();
-        System.out.println("From the numbers that were entered");
-        System.out.printf("The largest number is %d\n", (int)vr.getMaxInt(l));
-        System.out.printf("The smallest number is %d\n", (int)vr.getMinInt(l));
-        System.out.printf("The average of the numbers is %d\n", (int)vr.getAverageInt(l));
-
         
+        try {
+            for (int i = 0; i < 10; i++) {
+                l.add(input.nextInt());
+            }
+        } catch (InputMismatchException e) {
+            // System.out.println(e.getMessage());
+            System.out.println("ERROR: You can only enter integers");
+        }
+        
+        try {
+            System.out.println();
+            System.out.println("From the numbers that were entered");
+            System.out.printf("The largest number is %d\n", (int) vr.getMaxInt(l));
+            System.out.printf("The smallest number is %d\n", (int) vr.getMinInt(l));
+            System.out.printf("The average of the numbers is %d\n", (int) vr.getAverageInt(l));
+        } catch (ArithmeticException e) {
+            System.out.println("ERROR: A non integer value got though");
+        }
     }
 }
