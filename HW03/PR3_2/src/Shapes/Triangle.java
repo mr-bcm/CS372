645847a95650b79cf08c18f076a4cd96f6b3754d
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-12 18:36:40 -0800

    PR3_2 Triangle Class Updated

--- HW03/PR3_2/src/Shapes/Triangle.java
+++ HW03/PR3_2/src/Shapes/Triangle.java
@@ -3,27 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Shapes;
+import java.io.*;
 
 /**
  *
  * @author bmetzelaar16
  */
 public class Triangle {
+
     int s1, s2, s3;
-    
-    public Triangle (int s1, int s2, int s3){
+
+    public Triangle(int s1, int s2, int s3) throws Exception {
+        
+        if ((s1 <= 0) || (s2 <= 0) || (s3 <= 0)) {
+            throw new Exception ("A side cannot be 0 or less.\n");
+        } else if (s1 + s2 > s3) { // Triangle Inequality Theorem
+            throw new Exception ("The sum of side 1 and 2 cannot be greater than side 3.\n");
+        } else if (s1 + s3 > s2) { // -
+            throw new Exception ("The sum of side 1 and 3 cannot be greater than side 2.\n");
+        } else if (s2 + s3 > s1) { // -
+            throw new Exception ("The sum of side 2 and 3 cannot be greater than side 1.\n");
+        }
+
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
     }
-    
+
     public double getArea() {
         return 0;
     }
-    
+
     public double getPerimeter() {
         return s1 + s2 + s3;
     }
+
+    @Override
+    public String toString() {
+        return "A Legal Triangle";
+    }
+
 }
--- HW03/PR3_2/src/pr3_2/PR3_2.java
+++ HW03/PR3_2/src/pr3_2/PR3_2.java
@@ -17,9 +17,16 @@ public class PR3_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try{
         Rectangle rectangle = new Rectangle(5, 8);
+        Triangle triangle = new Triangle(0, 5, 4);
         
         System.out.println(rectangle.getArea());
+        System.out.println(triangle.getPerimeter());
+        } catch (Exception e){
+            System.out.print("Error: ");
+            System.out.println(e.getMessage());
+        }
     }
     
 }
