9e161616b9c6961d2d1034247f738ab757ec640a
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-13 15:00:49 -0800

    FileIOTest created Event class

---
+++ Playground/New Folder/FileIOTest/src/fileiotest/Event.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fileiotest;
+
+/**
+ *
+ * @author bmetzelaar16
+ */
+public class Event {
+    String name = "";
+    String location = "";
+    int month = 0;
+    int day = 0;
+    int year = 0;
+}
