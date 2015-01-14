fdae3a37efa37c3bea538e4a00b720e18bde550e
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-14 15:13:38 -0800

    Added Exam1
    Backing up work

---
+++ Exams/Exam1/src/exam1/Dice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exam1;
+import java.util.Random;
+
+/**
+ *
+ * @author bmetzelaar16
+ */
+public class Dice {
+    int sides = 0; // number of sides the dice has
+    Random rnd = new Random();
+    
+    public Dice(int sides){
+        this.sides = sides;
+    }
+    
+    public int roll(){
+        return (rnd.nextInt(sides) + 1);
+    }
+}
