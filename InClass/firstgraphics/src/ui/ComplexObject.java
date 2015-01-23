08e21c47fe7f052038173fa6444fbda4e58b2946
Brennan <bmetzelaar16@my.whitworth.edu>
2015-01-23 12:14:14 -0800

    Added Better Sized Images
    Smaller in size and the background is now transparent.

--- InClass/firstgraphics/src/ui/ComplexObject.java
+++ InClass/firstgraphics/src/ui/ComplexObject.java
@@ -19,31 +19,33 @@ import java.net.URL;
 import javax.imageio.ImageIO;
 
 public class ComplexObject {
-	BufferedImage img = null;
-	
-	public void draw(Graphics2D g, int x, int y) {
-        g.draw(new Line2D.Double(x, y, x+50, y+50));
-        
+
+    BufferedImage img = null;
+
+    public void draw(Graphics2D g, int x, int y) {
+        g.draw(new Line2D.Double(x, y, x + 50, y + 50));
+
         Paint currPaint = g.getPaint();
         g.setPaint(Color.BLACK);
-        g.draw(new Rectangle2D.Double(x+50, y+50, 50, 50));
+        g.draw(new Rectangle2D.Double(x + 50, y + 50, 50, 50));
         g.setPaint(Color.RED);
-        g.fill(new Rectangle2D.Double(x+51, y+51, 49, 49));
-        
-        g.setPaint(new GradientPaint(x+100, y+100, Color.RED, x+150, y+100, Color.BLACK));
-        g.fill(new RoundRectangle2D.Double(x+100, y+100, 50, 50, 10, 10));
+        g.fill(new Rectangle2D.Double(x + 51, y + 51, 49, 49));
+
+        g.setPaint(new GradientPaint(x + 100, y + 100, Color.RED, x + 150, y + 100, Color.BLACK));
+        g.fill(new RoundRectangle2D.Double(x + 100, y + 100, 50, 50, 10, 10));
         g.setPaint(currPaint);
 
         if (img == null) {
-	        try {
-	            URL url = this.getClass().getClassLoader().getResource("resources/mac.png");    	
-	            img = ImageIO.read(url);
-	        }
-	        catch (Exception ex) {}
+            try {
+                URL url = this.getClass().getClassLoader().getResource("resources/mac.png");
+                img = ImageIO.read(url);
+            } catch (Exception ex) {
+            }
         }
-        if (img != null)
-        	g.drawImage(img, null, x, y+50);
-        
+        if (img != null) {
+            g.drawImage(img, null, x, y + 50);
+        }
+
         g.draw(new Ellipse2D.Double(x, y, 150, 150));
-	}
+    }
 }
